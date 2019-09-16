5a3870eb984fd0e45139d5121d4b54d18bc3a916
williamorim <thewilliam89@gmail.com>
2019-09-16 15:08:39 -0300

    arrumando erros

--- exemplo-poluicao/importando.R
+++ exemplo-poluicao/importando.R
@@ -1,16 +1,6 @@
 # antes de continuarmos, vamos instalar pacotes!!!
 install.packages("tidyverse")
-install.packages("readxl")
 install.packages("writexl")
-install.packages("leaflet")
-install.packages("shiny")
-install.packages("remotes")
-install.packages("glmnet")
-install.packages("recipes")
-install.packages("caret")
-install.packages("ranger")
-install.packages("flexdashboard")
-install.packages("googledrive")
 
 # 1. Vamos carregar uma tabela do Excel -----------------------------------
 
